65fb58c6e608a76c271e8287aa2ee05b95b5d7bd
M. Neuber <mneuber55@gmail.com>
2018-11-09 16:21:11 -0500

    base file structure

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function() {
+    },
+
+    insertOne: function() {
+    },
+
+    updateOne: function() {
+    },
+}
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California', false);
