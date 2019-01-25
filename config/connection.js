ed1100c91fdda67e2b4e8f988f9a30ce123ec8f1
M. Neuber <mneuber55@gmail.com>
2019-01-25 16:50:46 -0500

    jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.json")[env];
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+if (config.use_env_variable) {
+  var connection = mysql.createConnection(process.env[config.use_env_variable]);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3307,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
